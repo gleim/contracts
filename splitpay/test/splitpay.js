de6d7d690e3d23a02a3bd11f4b896eaa98239fcd
Bill Gleim <bgleim@gmail.com>
2016-03-24 14:08:31 -0700

    Initial test driver functional

--- splitpay/test/splitpay.js
+++ splitpay/test/splitpay.js
@@ -2,7 +2,8 @@ contract('SplitPay', function(accounts) {
   it("should validate that SplitPay is deployed and available", function(done) {
     var splitpay = SplitPay.at(SplitPay.deployed_address);
     assert.isTrue(true);
-  }).then(done).catch(done);
+    done();
+  });
 
   it("should initialize a new SplitPay and check that the initial variables are being set correctly", function(done) {
     var splitpay = SplitPay.at(SplitPay.deployed_address);
@@ -13,23 +14,6 @@ contract('SplitPay', function(accounts) {
             assert.equal(buyer, accounts[0], "SplitPay initiator addresses don't match");
           }).then(done).catch(done);
       }).catch(done);
-  }).catch(done);
-
-  it("should add two content creator addresses", function(done) {
-    var splitpay = SplitPay.at(SplitPay.deployed_address);
-
-    splitpay.addPayee( accounts[1] ).then(
-      function(splitpay) {
-        splitpay.addPayee( accounts[2] );
-      }).then(done).catch(done);
-    }).catch(done);
-  }).catch(done);
-
-  it("should trigger payment to two content creator addresses", function(done) {
-    var splitpay = SplitPay.at(SplitPay.deployed_address);
-
-    splitpay.payout(1000);
-    assert.isTrue(true);
-  }).then(done).catch(done);
+  });
 
 });
